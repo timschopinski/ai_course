d840ec921864dd7faeb113e4e175f9ee82fca74d
timschopinski <timschopinski@gmail.com>
2023-03-07 12:37:35 +0100

    mse calculation using for loop

--- lab1/for_students.py
+++ lab1/for_students.py
@@ -4,7 +4,7 @@ from numpy import ndarray
 from data import get_data, inspect_data, split_data
 from lab1.closed_form_solution import get_closed_form_solution
 from lab1.gradient import get_gradient_descent_values
-from lab1.mse import calculate_mse
+from lab1.mse import calculate_mse, calculate_mse_using_loop
 
 data = get_data()
 inspect_data(data)
@@ -30,6 +30,9 @@ theta_0, theta_1 = get_closed_form_solution(x_train, y_train)
 
 
 # TODO: calculate error
+mse = calculate_mse_using_loop(theta_0, theta_1, x_test, y_test)
+print(f'MSE with for loop: {mse}')
+
 mse = calculate_mse(theta_0, theta_1, x_test, y_test)
 print(f'MSE: {mse}')
 
@@ -66,4 +69,17 @@ plt.plot(x, y)
 plt.scatter(x_test_std, y_test_std)
 plt.xlabel('Weight (standardized)')
 plt.ylabel('MPG (standardized)')
+plt.show()
+
+theta_0, theta_1 = get_closed_form_solution(x_test_std, y_test_std)
+mse = calculate_mse(theta_0, theta_1, x_test_std, y_test_std)
+print(f'MSE: {mse}')
+
+# plot the regression line
+x = np.linspace(min(x_test_std), max(x_test_std), 100)
+y = theta_0 + theta_1 * x
+plt.plot(x, y)
+plt.scatter(x_test_std, y_test_std)
+plt.xlabel('Weight (standardized) CFS')
+plt.ylabel('MPG (standardized) CFS')
 plt.show()
--- lab1/mse.py
+++ lab1/mse.py
@@ -13,3 +13,21 @@ def calculate_mse(
     y_pred = theta_0 + theta_1 * x_data
     mse = np.mean((y_data - y_pred) ** 2)
     return mse
+
+def calculate_mse_using_loop(
+        theta_0: float,
+        theta_1: float,
+        x_data: ndarray,
+        y_data: ndarray
+) -> Any:
+
+    n = len(y_data)
+
+    sum_ = 0
+    for i in range(n):
+        y_pred = theta_0 + theta_1 * x_data[i]
+        y = y_data[i]
+        result = y - y_pred
+        sum_ += result ** 2
+    mse = sum_ / n
+    return mse
