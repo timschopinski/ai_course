08073753bff512bcbdf5abeee789ad82f25bb750
timschopinski <timschopinski@gmail.com>
2023-05-09 12:03:32 +0200

    k_means_pp update

--- kmeans/k_means.py
+++ kmeans/k_means.py
@@ -2,37 +2,29 @@ import numpy as np
 
 
 def initialize_centroids_forgy(data, k):
-    # Randomly select k data points as the initial centroids
     return data[np.random.choice(data.shape[0], k, replace=False), :]
 
 
 def initialize_centroids_kmeans_pp(data, k):
-    # Choose the first centroid randomly
     centroids = np.zeros((k, data.shape[1]))
     centroids[0] = data[np.random.choice(data.shape[0]), :]
-    # Choose the rest of the centroids using k-means++ algorithm
     for i in range(1, k):
         distances = np.zeros((data.shape[0], i))
         for j in range(i):
             distances[:, j] = np.sqrt(np.sum((data - centroids[j]) ** 2, axis=1))
-        min_distances = np.min(distances, axis=1)
-        sum_min_distances = np.sum(min_distances)
-        probabilities = min_distances / sum_min_distances
-        cum_probabilities = np.cumsum(probabilities)
-        random_num = np.random.rand()
-        index = np.where(cum_probabilities >= random_num)[0][0]
+
+        max_distances = np.sum(distances, axis=1)
+        index = np.argmax(max_distances)
         centroids[i] = data[index]
     return centroids
 
 
 def assign_to_cluster(data, centroids):
-    # Find the closest centroid for each data point
     distances = np.sqrt(np.sum((data[:, np.newaxis, :] - centroids) ** 2, axis=2))
     return np.argmin(distances, axis=1)
 
 
 def update_centroids(data, assignments):
-    # Find the mean of data points in each cluster
     return np.array(
         [
             np.mean(data[assignments == k], axis=0)
